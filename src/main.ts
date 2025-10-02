77c547d35987958ba0737a94727953eb5b86e405
ProgensGithub <128368509+ProgensGithub@users.noreply.github.com>
2025-10-02 17:05:54 +0200

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,6 @@ import { CommonModule } from '@angular/common';
 import { AuthService } from './services/auth.service';
 import { LoginWindowComponent } from './components/login-window.component';
 import { InfoWindowComponent } from './components/info-window.component';
-import { AuthService } from './services/auth.service';
-import { LoginWindowComponent } from './components/login-window.component';
-import { InfoWindowComponent } from './components/info-window.component';
-import { AuthService } from './services/auth.service';
-import { LoginWindowComponent } from './components/login-window.component';
-import { InfoWindowComponent } from './components/info-window.component';
-import { AuthService } from './services/auth.service';
-import { LoginWindowComponent } from './components/login-window.component';
-import { InfoWindowComponent } from './components/info-window.component';
-import { AuthService } from './services/auth.service';
-import { LoginWindowComponent } from './components/login-window.component';
-import { InfoWindowComponent } from './components/info-window.component';
 import { SkrzynkiTreeComponent } from './components/skrzynki-tree.component';
 import { DocumentsGridComponent } from './components/documents-grid.component';
 import { DocumentDetailsComponent } from './components/document-details.component';
@@ -450,8 +438,6 @@ export class App {
   cancelHideMenu() {
     if (this.hideMenuTimeout) {
       clearTimeout(this.hideMenuTimeout);
-    }
-  }
   toggleMenu() {
     this.showMenu = !this.showMenu;
   }
