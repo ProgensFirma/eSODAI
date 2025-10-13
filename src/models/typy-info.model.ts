6911d5890b51b27ad6465d2ff7b610645e505855
Jarek ProgeNS <128368509+ProgensGithub@users.noreply.github.com>
2025-10-13 15:32:45 +0200

    Updated typy-info.model.ts

--- src/models/typy-info.model.ts
+++ src/models/typy-info.model.ts
@@ -1,25 +1,45 @@
-export interface TDokTypInfo {
-  nazwa: string;
-  finansowy: boolean;
-}
-
-export interface TKontrahentInfo {
-  numer: number;
-  identyfikator: string;
-  firma: boolean;
-  nIP: string;
-}
-
 export interface TOsobaInfo {
   numer: number;
   identyfikator: string;
 }
 
 export interface TWydzialInfo {
-  stanowisko: boolean;
   symbol: string;
   nazwa: string;
   kod: string;
+}
+
+export interface TKontrahentInfo {
+  numer: number;
+  identyfikator: string;
+  firma: boolean;
+  nip: string;
+  adres: string;
+}
+
+export interface TSprawaInfo {
+  numer: number;
+  znaksprawy: string;  
+  nazwa: string;
+  glowna: boolean;
+}
+
+export interface TDokTypInfo {
+  nazwa: string;
+  finansowy: boolean;
+  poleceniezaplaty: boolean;
+}
+
+export interface TSzablonInfo {
+  numer: number;
+  nazwa: string;
+  grupa: string;
+}
+
+export interface TPodpisDokumentInfo {
+  kolejnosc: number;
+  podpisal: TOsobaInfo;
+  data: string;
 }
 
 export interface TZalacznikInfo {
