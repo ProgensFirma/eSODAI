206fc56b500850fb0daf89c81b4d897b22b58369
Jarek ProgeNS <128368509+ProgensGithub@users.noreply.github.com>
2025-10-30 18:25:42 +0100

    Updated wykaz-akt.service.ts

--- src/services/wykaz-akt.service.ts
+++ src/services/wykaz-akt.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { WykazAkt } from '../models/wykaz-akt.model';
 import { ConfigService } from './config.service';
