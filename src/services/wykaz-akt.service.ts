25902d9ffb1c44f26fc069c267f8deff666fa818
Jarek ProgeNS <128368509+ProgensGithub@users.noreply.github.com>
2025-11-24 12:45:41 +0100

    Updated wykaz-akt.service.ts

--- src/services/wykaz-akt.service.ts
+++ src/services/wykaz-akt.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { WykazAkt } from '../models/wykaz-akt.model';
 import { ConfigService } from './config.service';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +14,16 @@ export class WykazAktService {
     return `${this.configService.apiBaseUrl}/wykazakt`;
   }
 
-  constructor(private http: HttpClient, private configService: ConfigService) {}
+  constructor(private http: HttpClient, 
+    private configService: ConfigService,
+    private authService: AuthService
+  ) {}
 
   getWykazAkt(): Observable<WykazAkt[]> {
+    const sesja = this.authService.getCurrentSession()?.sesja || 0;
 
     const params = new HttpParams()
-    .append('sesja', 123); 
+    .append('sesja', sesja); 
     
     return this.http.get<WykazAkt[]>(this.apiUrl, { params: params });
   }
