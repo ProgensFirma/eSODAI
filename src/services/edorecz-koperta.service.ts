fc8b852dc9534b134968b8caef57f7f2979c6209
Jarek ProgeNS <128368509+ProgensGithub@users.noreply.github.com>
2026-02-02 16:36:09 +0100

    Updated edorecz-koperta.service.ts

--- src/services/edorecz-koperta.service.ts
+++ src/services/edorecz-koperta.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { ConfigService } from './config.service';
 import { EdoreczPunktNadawczy } from '../models/edorecz-punkt-nadawczy.model';
 
@@ -10,8 +10,39 @@ import { EdoreczPunktNadawczy } from '../models/edorecz-punkt-nadawczy.model';
 export class EdoreczKopertaService {
   private http = inject(HttpClient);
   private configService = inject(ConfigService);
+  private useMockData = true;
+
+  private mockPunktyNadawcze: EdoreczPunktNadawczy[] = [
+    {
+      numer: 1,
+      nazwa: 'Punkt Nadawczy - Wydział Administracyjny',
+      domyslny: true,
+      oper: 'tboSelect',
+      status: 'sOK',
+      statusDane: ''
+    },
+    {
+      numer: 2,
+      nazwa: 'Punkt Nadawczy - Wydział Finansowy',
+      domyslny: false,
+      oper: 'tboSelect',
+      status: 'sOK',
+      statusDane: ''
+    },
+    {
+      numer: 3,
+      nazwa: 'Punkt Nadawczy - Sekretariat',
+      domyslny: false,
+      oper: 'tboSelect',
+      status: 'sOK',
+      statusDane: ''
+    }
+  ];
 
   getPunktyNadawcze(): Observable<EdoreczPunktNadawczy[]> {
+    if (this.useMockData) {
+      return of(this.mockPunktyNadawcze);
+    }
     const apiUrl = this.configService.apiBaseUrl;
     return this.http.get<EdoreczPunktNadawczy[]>(`${apiUrl}/eDorecz/PunktyNadawcze`);
   }
--- src/services/kontrah-wew.service.ts
+++ src/services/kontrah-wew.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { ConfigService } from './config.service';
 import { KontrahWew } from '../models/kontrah-wew.model';
 
@@ -10,8 +10,43 @@ import { KontrahWew } from '../models/kontrah-wew.model';
 export class KontrahWewService {
   private http = inject(HttpClient);
   private configService = inject(ConfigService);
+  private useMockData = true;
+
+  private mockDaneNadawcy: { [key: number]: KontrahWew } = {
+    1: {
+      numer: 1541968,
+      nazwa: 'GMINA SOFTRES - Wydział Administracyjny',
+      epuapSkrytka: '',
+      aDE: 'AE:PL-98699-88653-IIVRF-38',
+      oper: 'tboSelect',
+      status: 'sOK',
+      statusDane: ''
+    },
+    2: {
+      numer: 1541969,
+      nazwa: 'GMINA SOFTRES - Wydział Finansowy',
+      epuapSkrytka: '',
+      aDE: 'AE:PL-98699-88653-IIVRF-39',
+      oper: 'tboSelect',
+      status: 'sOK',
+      statusDane: ''
+    },
+    3: {
+      numer: 1541970,
+      nazwa: 'GMINA SOFTRES - Sekretariat',
+      epuapSkrytka: '',
+      aDE: 'AE:PL-98699-88653-IIVRF-40',
+      oper: 'tboSelect',
+      status: 'sOK',
+      statusDane: ''
+    }
+  };
 
   getDaneNadawcy(punktNumer: number): Observable<KontrahWew> {
+    if (this.useMockData) {
+      const mockData = this.mockDaneNadawcy[punktNumer] || this.mockDaneNadawcy[1];
+      return of(mockData);
+    }
     const apiUrl = this.configService.apiBaseUrl;
     return this.http.get<KontrahWew>(`${apiUrl}/KontrahWew`, {
       params: { punktNumer: punktNumer.toString() }
