13425ba3826860d94011b0ab4562580137b18316
Jarek ProgeNS <128368509+ProgensGithub@users.noreply.github.com>
2026-06-02 15:51:06 +0200

    Updated skrzynki.service.ts

--- src/services/skrzynki.service.ts
+++ src/services/skrzynki.service.ts
@@ -80,7 +80,7 @@ export class SkrzynkiService {
         zliczana: true,
         ilosc: 3,
         suma: 0,
-        zmiana: true,
+        zmiana: false,
         doWgl: false,
         doUsun: false,
         korEl: false,
