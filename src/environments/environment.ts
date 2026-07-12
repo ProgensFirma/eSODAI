0954376a15d42e08e3c8d741854592409608cd25
Jarek ProgeNS <128368509+ProgensGithub@users.noreply.github.com>
2026-07-12 19:30:21 +0200

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   frontVersion: '1.0.2.0',
-  SkrzynkiDokumentySprawyZalInfo: true,
+  skrzynkiDokumentySprawyZalInfo: true,
   pokazPelnyOpisBledu: true
 };
